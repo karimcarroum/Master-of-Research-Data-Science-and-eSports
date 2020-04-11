a777504b53e472ba2bdf60d9d60a6a2cacf234eb
karimcarroum <62953024+karimcarroum@users.noreply.github.com>
2020-04-11 17:15:34 +0200

    Update k fold cross validated neural network function.py

--- k fold cross validated neural network function.py
+++ k fold cross validated neural network function.py
@@ -12,6 +12,8 @@ from keras.layers import Dense
 
 from sklearn.model_selection import KFold
 
+from datetime import datetime
+
 
 # Import train set
 
@@ -36,7 +38,14 @@ def create_model(inputs, hu1, hu2, hu_last, actfun1, actfun2, actfun_last, optim
 
 def neural_network_reg_cv(train_x, train_y, inputs, hu1, hu2, hu_last, actfun, metric, loss, optimizer, folds, epochs = 20, batch_size = 20):
     
-    cv_mae = [] # np.zeros(len[actfun] ** 3 * hu1 * hu2 * hu_last
+    # Some indicators of runtime
+    start_time = datetime.now()
+    count = 0   # Updated after performing an iteration of the lowest level
+    iterations = hu1 * hu2 * hu_last * len(actfun) ** 3 * folds
+    print(iterations)
+    
+    # Results initial vectors 
+    cv_mae = []
     
     for hui in range(1, hu1 + 1):
         for huj in range(1, hu2 + 1):
@@ -52,26 +61,33 @@ def neural_network_reg_cv(train_x, train_y, inputs, hu1, hu2, hu_last, actfun, m
                                                      actfun_last=actfun_last, optimizer=optimizer, metric=metric, loss=loss)
                                 model.fit(cv_train_x, cv_train_y, epochs=epochs, batch_size=batch_size)
                                 folds_scores.append(model.evaluate(cv_test_x,cv_test_y))
+                            count = count + 1
+                            print(count, iterations / folds)
                             cv_mae.append(np.mean(folds_scores))
-    print("Lowest" + folds + "- fold CV mean absolute error is " + np.min(cv_mae))
-    return np.min(cv_mae), cv_mae
+                        
+    min_mae = np.min(cv_mae)
+    end_time = datetime.now()
+    total_time = end_time - start_time
+    return min_mae, cv_mae, total_time
                 
 
 # Model's hyperparameters // Model with 2 hidden layers, regression
 
 actfun = ["sigmoid", "tanh", "linear"]  # Activation functions
 inputs = train.shape[1] - 1             # Number of training variables
-hu1 = 2                                 # 1st hidden layer's maximum units
+hu1 = 3                                 # 1st hidden layer's maximum units
 hu2 = 2                                 # 2nd hidden layer's maximum units
 hu_last = 1                             # Hidden units in output layer --> 1 for regression, more for classification
 optimizer = "adam"                      # "adam", "rmsprop", "sgd"
 metric = [metrics.mae]                  # Performance metric
 loss = losses.mean_absolute_error       # Performance metric
-folds = 10
+folds = 2
 epochs = 20
-batch_size = 20
+batch_size = 50
 
 # Results
 
-neural_network_reg_cv(train_x=train_x, train_y=train_y, inputs=inputs, hu1=hu1, hu2=hu2, hu_last=hu_last, actfun=actfun, metric=metric,
+np.random.seed(1)
+cv_neural_net = neural_network_reg_cv(train_x=train_x, train_y=train_y, inputs=inputs, hu1=hu1, hu2=hu2, hu_last=hu_last, actfun=actfun, metric=metric,
                       loss=loss, optimizer=optimizer, folds=folds, epochs=epochs, batch_size=batch_size)
+print(cv_neural_net)
